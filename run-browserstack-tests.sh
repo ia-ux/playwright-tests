c6f60063d850a0eb0e78e3e4df17007d1d16f83f
clau <1281581+dualcnhq@users.noreply.github.com>
2025-04-11 06:59:06 +0800

    update GH workflow

--- run-browserstack-tests.sh
+++ run-browserstack-tests.sh
@@ -1,16 +1,17 @@
 TIMEFORMAT='It took %0R seconds.' 
 time {
-  npx browserstack-node-sdk playwright test 
-  # npx browserstack-node-sdk playwright test ./tests/av
-  # npx browserstack-node-sdk playwright test ./tests/about
-  # npx browserstack-node-sdk playwright test ./tests/books
-  # npx browserstack-node-sdk playwright test ./tests/collection
+  # npx browserstack-node-sdk playwright test - this is failing due to timeout
+  npx browserstack-node-sdk playwright test ./tests/av
+  npx browserstack-node-sdk playwright test ./tests/about
+  npx browserstack-node-sdk playwright test ./tests/books
+  npx browserstack-node-sdk playwright test ./tests/collection
   # npx browserstack-node-sdk playwright test ./tests/details
-  # npx browserstack-node-sdk playwright test ./tests/donation
+  npx browserstack-node-sdk playwright test ./tests/donation
   # npx browserstack-node-sdk playwright test ./tests/home
-  # npx browserstack-node-sdk playwright test ./tests/login
+  npx browserstack-node-sdk playwright test ./tests/legal
+  npx browserstack-node-sdk playwright test ./tests/login
   # npx browserstack-node-sdk playwright test ./tests/music
   # npx browserstack-node-sdk playwright test ./tests/profile
   # npx browserstack-node-sdk playwright test ./tests/search
-  # npx browserstack-node-sdk playwright test ./tests/static
+  npx browserstack-node-sdk playwright test ./tests/static
 }
