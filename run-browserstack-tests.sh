bba844b013dceb95ea9c6125c9930ad43b3e199e
clau <1281581+dualcnhq@users.noreply.github.com>
2026-02-27 03:40:29 +0800

    update config and browserstack version

--- run-browserstack-tests.sh
+++ run-browserstack-tests.sh
@@ -1,30 +1,31 @@
 TIMEFORMAT='It took %0R seconds.' 
 time {
-  # npx browserstack-node-sdk playwright test - this is failing due to timeout
+  npx browserstack-node-sdk playwright test # this might fail due to timeout
 
-  echo "\nRun ./tests/av"
-  npx browserstack-node-sdk playwright test ./tests/av
 
-  echo "\nRun ./tests/about"
-  npx browserstack-node-sdk playwright test ./tests/about
+  # echo "\nRun ./tests/av"
+  # npx browserstack-node-sdk playwright test ./tests/av
 
-  echo "\nRun ./tests/books"
-  npx browserstack-node-sdk playwright test ./tests/books
+  # echo "\nRun ./tests/about"
+  # npx browserstack-node-sdk playwright test ./tests/about
 
-  echo "\nRun ./tests/collection"
-  npx browserstack-node-sdk playwright test ./tests/collection
+  # echo "\nRun ./tests/books"
+  # npx browserstack-node-sdk playwright test ./tests/books
+
+  # echo "\nRun ./tests/collection"
+  # npx browserstack-node-sdk playwright test ./tests/collection
 
   # echo "\nRun ./tests/details"
   # npx browserstack-node-sdk playwright test ./tests/details
 
-  echo "\nRun ./tests/donation"
-  npx browserstack-node-sdk playwright test ./tests/donation
+  # echo "\nRun ./tests/donation"
+  # npx browserstack-node-sdk playwright test ./tests/donation
 
-  echo "\nRun ./tests/home"
-  npx browserstack-node-sdk playwright test ./tests/home
+  # echo "\nRun ./tests/home"
+  # npx browserstack-node-sdk playwright test ./tests/home
 
-  echo "\nRun ./tests/legal"
-  npx browserstack-node-sdk playwright test ./tests/legal
+  # echo "\nRun ./tests/legal"
+  # npx browserstack-node-sdk playwright test ./tests/legal
 
   # echo "\nRun ./tests/login"
   # npx browserstack-node-sdk playwright test ./tests/login
@@ -32,15 +33,15 @@ time {
   # echo "Run ./tests/music"
   # npx browserstack-node-sdk playwright test ./tests/music
 
-  echo "\nRun ./tests/openlibrary"
-  npx browserstack-node-sdk playwright test ./tests/openlibrary
+  # echo "\nRun ./tests/openlibrary"
+  # npx browserstack-node-sdk playwright test ./tests/openlibrary
 
   # echo "\nRun ./tests/profile"
   # npx browserstack-node-sdk playwright test ./tests/profile
 
-  echo "Run ./tests/search"
-  npx browserstack-node-sdk playwright test ./tests/search
+  # echo "Run ./tests/search"
+  # npx browserstack-node-sdk playwright test ./tests/search
 
-  echo "\nRun ./tests/static"
-  npx browserstack-node-sdk playwright test ./tests/static
+  # echo "\nRun ./tests/static"
+  # npx browserstack-node-sdk playwright test ./tests/static
 }
