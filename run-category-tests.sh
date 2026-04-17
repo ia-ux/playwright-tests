c9428c1c4d21cb0b35bb1f68050492fa870f89e8
clau <1281581+dualcnhq@users.noreply.github.com>
2026-04-18 03:58:53 +0800

    use playwright v1.58.1

--- run-category-tests.sh
+++ run-category-tests.sh
@@ -4,11 +4,11 @@ time {
    npm run test -- --test=about
    npm run test -- --test=books
    npm run test -- --test=collection
-  #  npm run test -- --test=details
+   npm run test -- --test=details
   #  npm run test -- --test=donation
    npm run test -- --test=home
-  #  npm run test -- --test=login
-  #  npm run test -- --test=music
-  #  npm run test -- --test=profile
-  #  npm run test -- --test=search
+   npm run test -- --test=login
+   npm run test -- --test=music
+   npm run test -- --test=profile
+   npm run test -- --test=search
 }
